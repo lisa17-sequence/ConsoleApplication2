816695cd68246744edb7ac5d15c3d5df076956eb
Lisa <ellissaveta17@gmail.com>
2022-11-10 15:01:14 +0100

    update

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -3,45 +3,18 @@
 #include <iostream>
 #include <set>
 #include <string>
-void MakeSin(int limit) {
-	std::set < std::pair<double, double>> a;
-	sf::RenderWindow window(sf::VideoMode(500, 500), "window");
-	sf::CircleShape point(10.f);
-	point.setFillColor(sf::Color::Red);
-	int countOfDots = 361;
+#include <vector>
 
-	while (window.isOpen()) {
-		int x0 = 0;
-		int y0 = 0;
-		sf::Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-
-		window.clear();
-		for (double i = 0; i <= countOfDots; i = i + 30) {
-			float x = i;
-			float y = limit * sin(i * (6.284 / 360.0));
-
-			float x1 = x0 + x;
-			float y1 = y0 + y;
-
-			a.insert({ x1, y1 });
-			point.setPosition(x1, y1);
-			window.draw(point);
-		}
-		window.display();
+std::vector < std::pair<double, double>> MakeSin (double leftLimit, double rightLimit, int step) {
+	std::vector < std::pair<double, double>> table;
+	for (double x = leftLimit; x <= rightLimit; x += step) {
+		table.push_back({ x, sin(x) });
+		std::cout << x << ' ' << sin(x) << '\n';
 	}
-
-
-	for (auto i : a) {
-		std::cout << i.first << ' ' << i.second << '\n';
-	}
+	return table;
 }
 int main() {
-	
-	MakeSin(10);
+	MakeSin(-5, 5, 1);
 }
 
 
