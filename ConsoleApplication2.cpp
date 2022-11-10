99f14d9bff45befccebc6013464b7b873e588816
Lisa <ellissaveta17@gmail.com>
2022-11-10 15:11:34 +0100

    update

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -5,7 +5,9 @@
 #include <string>
 #include <vector>
 
-std::vector < std::pair<double, double>> MakeSin (double leftLimit, double rightLimit, int step) {
+std::vector < std::pair<double, double>> MakeSin (double leftLimit, double rightLimit, double userSteps) {
+	double allSteps = abs(leftLimit) + abs(rightLimit) + 1;
+	double step = allSteps / userSteps;
 	std::vector < std::pair<double, double>> table;
 	for (double x = leftLimit; x <= rightLimit; x += step) {
 		table.push_back({ x, sin(x) });
@@ -14,7 +16,7 @@ std::vector < std::pair<double, double>> MakeSin (double leftLimit, double right
 	return table;
 }
 int main() {
-	MakeSin(-5, 5, 1);
+	MakeSin(-5, 6, 6);
 }
 
 
