2195165c5b84132c4f88e58de337e35541669956
Lisa <ellissaveta17@gmail.com>
2022-11-11 11:37:08 +0100

    update

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -2,20 +2,22 @@
 #include <vector>
 #include <iostream>
 
-auto MakeSin(const double upLimit)
+auto MakeSin(const double downLimit, const double upLimit, const double countOfDots)
 {
 	constexpr double leftLimit = -4;
 	constexpr double rightLimit = 4;
+	const double allSteps = abs(leftLimit) + abs(rightLimit) + 1;
+	const double step = allSteps / countOfDots;
 	std::vector<std::pair<double, double>> table;
-	for (double x = leftLimit; x <= rightLimit; x++)
+	for (double x = leftLimit; x <= rightLimit; x += step)
 	{
-		table.emplace_back(x, (upLimit*(sin(x))));
-		std::cout << x << ' ' << upLimit* sin(x) << '\n';
+		table.emplace_back(x, upLimit * sin(x));
+		std::cout << x << ' ' << upLimit * sin(x) << '\n';
 	}
 	return table;
 }
 
 int main()
 {
-	MakeSin( 10);
+	MakeSin(-10, 10, 9);
 }
